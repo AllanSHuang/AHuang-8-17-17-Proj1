8622b95693b90cc77b6aa83924dfc775cb3f40fc
DoctorDank <huangallan853@gmail.com>
2017-08-18 08:47:21 -0700

    Hello World Demonstration
    In this first program, I only wrote a single class project that contains
    a main method. And inside the main method there is a statement saying
    "Hello World" and the program print it out on the terminal window.
    Pretty easy project.

---
+++ Hello_World/src/Hello_World.java
@@ -0,0 +1,11 @@
+//This program displays a statement saying(hello world)
+//Programmed by Allan Huang
+//Programmed on 8/17/2017
+public class Hello_World 
+{
+	public static void main(String[]args)
+	{
+	  System.out.print("Hello World");
+		
+	}
+}
