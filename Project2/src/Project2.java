6720dda324eb8c69edddfed6c706956a5fcd362b
DoctorDank <huangallan853@gmail.com>
2017-08-22 09:29:49 -0700

    initial commit

---
+++ Project2/src/Project2.java
@@ -0,0 +1,16 @@
+//Programmed by Allan Huang
+//The second project, it compares print and println
+//August 22, 2017
+public class Project2 
+{
+  public static void main(String[]args)  
+  {
+	System.out.print("This program will show the difference between print and println");
+    System.out.println("\n");
+    System.out.print("Hello, ");
+    System.out.print("this is Allan from 1st period.");
+    System.out.println("\n");
+    System.out.println("Println move a statement to a new line every time");
+    System.out.println("SEE, it moves this statment to another line!");
+  }
+}
