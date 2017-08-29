46b9266dc8867c474d89a7a418ca44bd4172c83c
DoctorDank <huangallan853@gmail.com>
2017-08-29 08:10:53 -0700

    Proj 2 Difference between print/println.

--- Project2/src/Project2.java
+++ Project2/src/Project2.java
@@ -5,12 +5,12 @@ public class Project2
 {
   public static void main(String[]args)  
   {
-	System.out.print("This program will show the difference between print and println");
+	System.out.print("This program will show the difference between print and println:");
     System.out.println("\n");
-    System.out.print("Hello, ");
+    System.out.print("S.O.P: Hello, ");
     System.out.print("this is Allan from 1st period.");
     System.out.println("\n");
-    System.out.println("Println move a statement to a new line every time");
-    System.out.println("SEE, it moves this statment to another line!");
+    System.out.println("S.O.Println moves a statement to a new line every time:");
+    System.out.println("SEE? It moves this statment to another line!");
   }
 }
